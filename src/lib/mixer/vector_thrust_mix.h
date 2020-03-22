a629942bef8a5d5847cfbefc3930d91868c4fdfa
phmsmech <phmsmech@gmail.com>
2020-03-23 08:37:17 +1300

    incresed vec thurst scaling in mixer

--- src/lib/mixer/vector_thrust_mix.h
+++ src/lib/mixer/vector_thrust_mix.h
@@ -15,14 +15,14 @@ struct VectorThrustMix {
 //define columns of mixing matrix here
 //      { SCALE X,  SCALE Y}
 constexpr VectorThrustMix multirotor_vt_config[] = {   //add a line for each motor indicating scaling for each direction
-	{ -0.1767,  0.1767},	//Motor 1
-	{ -0.1767, -0.1767},	//Motor 2
-	{  0.1767, -0.1767},	//Motor 3
-	{  0.1767,  0.1767},	//Motor 4
-	{  0.1767,  0.1767},	//Motor 5
-	{  0.1767, -0.1767},	//Motor 6
-	{ -0.1767, -0.1767},	//Motor 7
-	{ -0.1767,  0.1767},	//Motor 8
+	{ -0.250,  0.250},	//Motor 1
+	{ -0.250, -0.250},	//Motor 2
+	{  0.250, -0.250},	//Motor 3
+	{  0.250,  0.250},	//Motor 4
+	{  0.250,  0.250},	//Motor 5
+	{  0.250, -0.250},	//Motor 6
+	{ -0.250, -0.250},	//Motor 7
+	{ -0.250,  0.250},	//Motor 8
 }; //See Airframes Reference for motor numbering -> Octorotor Coaxial
 
 // EXAMPLE FOR COAXIAL OCTO WITH TILTED PROPELLERS
